56ab58fa0a03200577f448333f01152869ed31d5
bladtherf <67644953+bladtherf@users.noreply.github.com>
2020-10-20 15:06:52 +0530

    update

---
+++ TPS/Assets/MAIN_SCRIPTS/transform.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class transform : MonoBehaviour
+{
+    
+    public Transform transformofGun;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.position = transformofGun.position;
+        transform.rotation = transformofGun.rotation;
+    }
+}
