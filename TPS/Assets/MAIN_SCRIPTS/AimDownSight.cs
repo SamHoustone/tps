334f0e17c96e3b195b972a33713f68e34c05e6e8
bladtherf <67644953+bladtherf@users.noreply.github.com>
2020-10-22 18:44:52 +0530

    update

--- TPS/Assets/MAIN_SCRIPTS/AimDownSight.cs
+++ TPS/Assets/MAIN_SCRIPTS/AimDownSight.cs
@@ -6,22 +6,32 @@ public class AimDownSight : MonoBehaviour
 {
     public Vector3 aimDownSights;
     public Quaternion aimDownSightsRotation;
+    public GameObject cam1;
+    public GameObject cam2;
     public Vector3 hipFire;
     public float aimspeed;
+    public Vector3 offset;
 
 
 
-    void Update()
+    void FixedUpdate()
     {
         if (Input.GetMouseButton(1))
         {
-            transform.localPosition = Vector3.Slerp(transform.localPosition, aimDownSights, aimspeed * Time.deltaTime);
-            transform.localRotation = Quaternion.Slerp(transform.rotation, aimDownSightsRotation, aimspeed * Time.deltaTime);
+            cam1.SetActive (false);
+            cam2.SetActive(true);
+           
+
+            
 
         }
         else
         {
+            cam1.SetActive(true);
+            cam2.SetActive(false);
             transform.localPosition = Vector3.Slerp(transform.localPosition, hipFire, aimspeed * Time.deltaTime);
         }
+
+        
     }
 }
